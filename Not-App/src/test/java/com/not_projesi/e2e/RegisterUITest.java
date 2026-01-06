c9e665a1d2cda89dd11d56998053836eb95cee04
Vedat Koçyiğit <116923628+vedatkocyigit@users.noreply.github.com>
2026-01-06 06:26:17 +0300

    Update RegisterUITest.java

--- Not-App/src/test/java/com/not_projesi/e2e/RegisterUITest.java
+++ Not-App/src/test/java/com/not_projesi/e2e/RegisterUITest.java
@@ -18,6 +18,7 @@ public class RegisterUITest {
     @Test
     void shouldRegisterUser() {
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         String uniqueId = UUID.randomUUID().toString().substring(0, 8);
         String testUsername = "testuser_" + uniqueId;
@@ -26,16 +27,27 @@ public class RegisterUITest {
         try {
             driver.get("http://localhost/register");
 
-            driver.findElement(By.name("username")).sendKeys(testUsername);
-            driver.findElement(By.name("ogrenciSifre")).sendKeys("123456");
-            driver.findElement(By.name("ogrenciAdi")).sendKeys("Test");
-            driver.findElement(By.name("ogrenciSoyadi")).sendKeys("User");
-            driver.findElement(By.name("ogrenciEmail")).sendKeys(testEmail);
-            driver.findElement(By.name("bolumId")).sendKeys("1");
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")))
+                    .sendKeys(testUsername);
 
-            driver.findElement(By.tagName("button")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("ogrenciSifre")))
+                    .sendKeys("123456");
 
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("ogrenciAdi")))
+                    .sendKeys("Test");
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("ogrenciSoyadi")))
+                    .sendKeys("User");
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("ogrenciEmail")))
+                    .sendKeys(testEmail);
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("bolumId")))
+                    .sendKeys("1");
+
+            wait.until(ExpectedConditions.elementToBeClickable(
+                    By.cssSelector("button[type='submit']")
+            )).click();
 
             WebElement msg = wait.until(
                     ExpectedConditions.visibilityOfElementLocated(
