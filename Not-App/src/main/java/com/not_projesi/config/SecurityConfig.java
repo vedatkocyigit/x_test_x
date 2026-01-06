d9ed52364315e864bc0056603b70393a51366e44
Vedat Koçyiğit <116923628+vedatkocyigit@users.noreply.github.com>
2026-01-06 06:14:29 +0300

    Update SecurityConfig.java

--- Not-App/src/main/java/com/not_projesi/config/SecurityConfig.java
+++ Not-App/src/main/java/com/not_projesi/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                                       .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers(
                                 "/auth/**",
                                 "/authenticate",
